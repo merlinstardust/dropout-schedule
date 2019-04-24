447615d056b942557b27121fe0c855a60dc12c97
Merlin <merlinpatt@gmail.com>
2019-04-24 04:51:14 -0400

    Updates

---
+++ precache-manifest.67bd0f5600d8c1c8fbf36b7b0a9793a4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dc10781e55a44de2390abd9baf79137b",
+    "url": "/dropout-schedule/index.html"
+  },
+  {
+    "revision": "1e883dc7645b0cfcbc18",
+    "url": "/dropout-schedule/static/css/main.bd1f912d.chunk.css"
+  },
+  {
+    "revision": "2315f74ce3c99316133d",
+    "url": "/dropout-schedule/static/js/2.3310f33a.chunk.js"
+  },
+  {
+    "revision": "1e883dc7645b0cfcbc18",
+    "url": "/dropout-schedule/static/js/main.0a6c0a27.chunk.js"
+  },
+  {
+    "revision": "5e90281b709fe93d8972",
+    "url": "/dropout-schedule/static/js/runtime~main.92729892.js"
+  }
+]);
